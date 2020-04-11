2550bcc3ca5c509653c291e5ff0bdf15840baeba
ibanez75612 <ibanez75612@gmail.com>
2020-04-11 13:31:00 +0300

    Admin panel projesi için area oluşturuldu ve layoutu giydirildi.

---
+++ ECommerce/ECommerce.WEB/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using ECommerce.WEB.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ECommerce.WEB.Areas.Admin.Controllers
+{
+    public class HomeController : BaseController
+    {
+        // GET: Admin/Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
