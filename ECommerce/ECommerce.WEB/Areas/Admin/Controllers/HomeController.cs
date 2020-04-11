6e462d6a78a517738539cd239e6a51159a7b1939
ibanez75612 <ibanez75612@gmail.com>
2020-04-11 17:03:19 +0300

    admin için attribute yazıldı ve admin login sayfası tasarlandıç

--- ECommerce/ECommerce.WEB/Areas/Admin/Controllers/HomeController.cs
+++ ECommerce/ECommerce.WEB/Areas/Admin/Controllers/HomeController.cs
@@ -14,5 +14,10 @@ namespace ECommerce.WEB.Areas.Admin.Controllers
         {
             return View();
         }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
     }
 }
---
+++ ECommerce/ECommerce.WEB/Utility/Attribute/LoggedAdminAttribute.cs
@@ -0,0 +1,39 @@
+﻿using ECommerce.WEB.Utility.UIFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace ECommerce.WEB.Utility.Attribute
+{
+    public class LoggedAdminAttribute: ActionFilterAttribute, IActionFilter
+    {
+        void IActionFilter.OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            string actionReturnType = (filterContext.ActionDescriptor as ReflectedActionDescriptor).MethodInfo.ReturnType.Name;
+
+            var sessionControl = filterContext.HttpContext.Session["LoggedUser"];
+            if (sessionControl == null)
+            {
+                if (actionReturnType == "ActionResult")
+                {
+                    filterContext.Result = new RedirectToRouteResult(new RouteValueDictionary { { "controller", "Home" }, { "action", "Index" },{ "Area","Admin"} });
+                }
+                else if (actionReturnType == "JsonResult")
+                {
+                    filterContext.Result = new JsonResult
+                    {
+                        Data = new Response()
+                        {
+                            Message = "Lütfen Giriş Yapınız",
+                            Status = false
+                        },
+                        JsonRequestBehavior = JsonRequestBehavior.AllowGet
+                    };
+                }
+            }
+        }
+    }
+}
