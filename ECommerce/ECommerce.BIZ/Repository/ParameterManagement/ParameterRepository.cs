68f3d643ed370fae0b2fcf861d539230d58f347f
ibanez75612 <ibanez75612@gmail.com>
2020-04-23 16:29:01 +0300

    Ürün Özelliğii CRUD şlemleri Tamamlandı.

--- ECommerce/ECommerce.BIZ/Repository/ParameterManagement/ParameterRepository.cs
+++ ECommerce/ECommerce.BIZ/Repository/ParameterManagement/ParameterRepository.cs
@@ -10,35 +10,50 @@ namespace ECommerce.BIZ.Repository.ParameterManagement
 {
     public class ParameterRepository : IRepository<Parameter>
     {
-		ECommerceEntities db;
-		public ParameterRepository()
-		{
-			db = new ECommerceEntities();
-		}
+        ECommerceEntities db;
+        public ParameterRepository()
+        {
+            db = new ECommerceEntities();
+        }
 
-		public List<Parameter> GetAll()
-		{
-			List<Parameter> parametersList = db.Parameter.Where(s => s.IsActive && !s.IsDeleted).ToList();
-			return parametersList;
-		}
+        public List<Parameter> GetAll()
+        {
+            List<Parameter> parametersList = db.Parameter.Where(s => s.IsActive && !s.IsDeleted).ToList();
+            return parametersList;
+        }
 
-		public Parameter GetById(int id)
-		{
-			Parameter parameter = db.Parameter.Where(s => s.ParameterId == id && s.IsActive && !s.IsDeleted).SingleOrDefault();
-			return parameter;
-		}
+        public Parameter GetById(int id)
+        {
+            Parameter parameter = db.Parameter.Where(s => s.ParameterId == id && s.IsActive && !s.IsDeleted).SingleOrDefault();
+            return parameter;
+        }
 
-		public void Insert(Parameter entity)
-		{
-			db.Parameter.Add(entity);
-			db.SaveChanges();
-		}
+        public void Insert(Parameter entity)
+        {
+            db.Parameter.Add(entity);
+            db.SaveChanges();
+        }
 
-		public void Update(Parameter entity)
-		{
-			db.Entry(entity).State = System.Data.Entity.EntityState.Modified;
-			db.SaveChanges();
-		}
+        public void Update(Parameter entity)
+        {
+            db.Entry(entity).State = System.Data.Entity.EntityState.Modified;
+            db.SaveChanges();
+        }
 
-	}
+        public List<Parameter> GetAllListByParameterTypeName(string parameterTypeName)
+        {
+            ParameterType parameterType = db.ParameterType.Where(s => s.Name.Equals(parameterTypeName) && s.IsActive && !s.IsDeleted).SingleOrDefault();
+
+            List<Parameter> parameterList = db.Parameter.Where(s => s.ParameterTypeId== parameterType.ParameterTypeId && s.IsActive && !s.IsDeleted).ToList();
+            return parameterList;
+        }
+
+        public Parameter GetByParameterNameAndParameterTypeId(string parameterName, int parameterTypeId)
+        {
+            Parameter parameter = db.Parameter.Where(s => s.Name.Equals(parameterName) && s.ParameterTypeId == parameterTypeId).SingleOrDefault();
+
+            return parameter;
+        }
+
+    }
 }
