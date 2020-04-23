c54148ec6ddaa237efa8125fb8bf387cc460c738
Mısra Erol <misraerol@gmail.com>
2020-04-23 14:00:37 +0300

    Kategori CRUD işlemleri yapıldı.

--- ECommerce/ECommerce.WEB/Areas/Admin/Controllers/CategoryManagement/CategoryController.cs
+++ ECommerce/ECommerce.WEB/Areas/Admin/Controllers/CategoryManagement/CategoryController.cs
@@ -25,10 +25,10 @@ namespace ECommerce.WEB.Areas.Admin.Controllers.CategoryManagement
         // GET: Admin/Category
         public ActionResult Index()
         {
-            List<CategoryListView> categoryListView = categoryRepository.GetAllCategoryList().Select(a=>new CategoryListView
+            List<CategoryListView> categoryListView = categoryRepository.GetAllCategoryList().Select(a => new CategoryListView
             {
-                CategoryId=a.LastCategoryId,
-                Name=a.Name
+                CategoryId = a.LastCategoryId,
+                Name = a.Name
             }).ToList();
             return View(categoryListView);
         }
@@ -53,7 +53,7 @@ namespace ECommerce.WEB.Areas.Admin.Controllers.CategoryManagement
                 TopCategoryId = categoryCRUDModel.TopCategoryId == null ? 0 : categoryCRUDModel.TopCategoryId.Value,
                 Name = categoryCRUDModel.CategoryName
             };
- 
+
 
             categoryRepository.Insert(category);
             Response response = new Response()
@@ -64,6 +64,63 @@ namespace ECommerce.WEB.Areas.Admin.Controllers.CategoryManagement
             };
             return Json(response);
         }
-
+        [HttpGet]
+        public ActionResult Update(int id = 0)
+        {
+            CategoryCRUDModel categoryCRUDModel = new CategoryCRUDModel();
+            Category category = categoryRepository.GetById(id);
+            if (category != null)
+            {
+                categoryCRUDModel.CategoryId = category.CategoryId;
+                categoryCRUDModel.CategoryName = category.Name;
+                categoryCRUDModel.TopCategoryId = category.TopCategoryId;
+                List<RecursiveCategoryList_Result> recursiveCategoryList = categoryRepository.GetAllCategoryList();
+                categoryCRUDModel.TopCategoryList = new SelectList(recursiveCategoryList, "LastCategoryId", "Name");
+                return View(categoryCRUDModel);
+            }
+            else
+            {
+                return RedirectToAction("Index", "Category");
+            }
+        }
+        [HttpPost]
+        public ActionResult Update(CategoryCRUDModel categoryCRUDModel)
+        {
+            Response response;
+            Category category = categoryRepository.GetById(categoryCRUDModel.CategoryId);
+            if (category != null)
+            {
+                category.CategoryId = categoryCRUDModel.CategoryId;
+                category.Name = categoryCRUDModel.CategoryName;
+                category.TopCategoryId = categoryCRUDModel.TopCategoryId == null ? 0 : categoryCRUDModel.TopCategoryId.Value;
+                categoryRepository.Update(category);
+                response = new Response()
+                {
+                    Message = "Kayıt Güncellendi",
+                    RedirectUrl = Url.Action("Index", "Category"),
+                    Status = true
+                };
+            }
+            else
+            {
+                response = new Response()
+                {
+                    Message = "Güncelleme işlemi başarısız",
+                    Status = false
+                };
+            }
+            return Json(response);
+        }
+        public ActionResult Delete(int id)
+        {
+            Category category = categoryRepository.GetById(id);
+            if(category != null)
+            {
+                category.IsActive = false;
+                category.IsDeleted = true;
+                categoryRepository.Update(category);
+            }
+            return RedirectToAction("Index", "Category");
+        }
     }
 }
