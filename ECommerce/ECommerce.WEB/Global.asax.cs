b8d61c9fef466388a3075c86f5cc926ba927a178
ibanez75612 <ibanez75612@gmail.com>
2020-04-29 19:01:28 +0300

    globalasaxda beni hatırla ile ilgili işlem yapıldı

--- ECommerce/ECommerce.WEB/Global.asax.cs
+++ ECommerce/ECommerce.WEB/Global.asax.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ECommerce.BIZ.Repository.AppUserManagement;
+using ECommerce.DATA;
+using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
@@ -6,6 +8,7 @@ using System.Threading;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using System.Web.SessionState;
 
 namespace ECommerce.WEB
 {
@@ -17,6 +20,37 @@ namespace ECommerce.WEB
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             GlobalFilters.Filters.Add(new ValidateInputAttribute(false));//Validate Input false için yazıldı
         }
- 
+
+        protected void Application_PreRequestHandlerExecute(object sender, EventArgs e)
+        {
+            if (Context.Handler is IRequiresSessionState || Context.Handler is IReadOnlySessionState)
+            {
+                HttpContext context = HttpContext.Current;
+
+                if (HttpContext.Current.Session["LoggedUser"] == null)
+                {
+                    HttpCookie cookie = HttpContext.Current.Request.Cookies["AppUser"];
+                    if (cookie != null)
+                    {
+                        AppUserRepository appUserRepository = new AppUserRepository();
+
+
+                        string email = cookie.Values["email"];
+
+                        string password = cookie.Values["password"];
+
+                        AppUser appUser = appUserRepository.GetByEmailAndPassword(email, password);
+
+                        if (appUser != null)
+                        {
+                            Session.Add("LoggedUser", appUser);
+
+                        }
+                    }
+                }
+
+            }
+        }
+
     }
 }
