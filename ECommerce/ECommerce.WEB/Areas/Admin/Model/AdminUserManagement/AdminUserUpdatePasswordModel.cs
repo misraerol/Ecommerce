78a02907a82476be082c144ec6b0c7a364391cbb
Mısra Erol <misraerol@gmail.com>
2020-05-01 15:04:32 +0300

    AdminUser  aktivasyon işlemleri yapıldı.

---
+++ ECommerce/ECommerce.WEB/Areas/Admin/Model/AdminUserManagement/AdminUserUpdatePasswordModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ECommerce.WEB.Areas.Admin.Model.AdminUserManagement
+{
+    public class AdminUserUpdatePasswordModel
+    {
+        public int AdminUserId { get; set; }
+
+        [DisplayName("Şifre")]
+        [Required]
+        public string Password { get; set; }
+
+
+        [DisplayName("Şifre Tekrar")]
+        [Required]
+        public string RePassword { get; set; }
+
+        public string ActivationCode{ get; set; }
+    }
+}
